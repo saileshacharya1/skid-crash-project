2ab8430a5c20407745f863c9253d065b48cec756
Sailesh Acharya <sailesh.acharya@usu.edu>
2023-03-30 15:18:18 -0600

    Addding crash types.

--- code/main.R
+++ code/main.R
@@ -390,25 +390,41 @@ df$COUNT_MILE <- df$COUNT_TOT / df$LENGTH
 ### Some visualizations ########################################################
 ################################################################################
 
-# select route
-df1 <- df
-df <- df1[df1$LABEL != "0089",]
+# select route: I-15
+df_15 <- df[df$LABEL == "0015N" | df$LABEL == "0015P",]
 
 # correlation between SN and crash count
-cor(df$SN, df$COUNT_MILE)
-plot(df$SN, df$COUNT_MILE)
-abline(lm(df$COUNT_MILE ~ df$SN), col = 4, lwd = 3)
+cor(df_15$SN, df_15$COUNT_MILE)
+plot(df_15$SN, df_15$COUNT_MILE)
+abline(lm(df_15$COUNT_MILE ~ df_15$SN), col = 4, lwd = 3)
 
 # correlation between SN and AADT
-cor(df$SN, df$AADT)
-plot(df$SN, df$AADT)
-abline(lm(df$AADT ~ df$SN), col = 4, lwd = 3)
-
+cor(df_15$SN, df_15$AADT)
+plot(df_15$SN, df_15$AADT)
+abline(lm(df_15$AADT ~ df_15$SN), col = 4, lwd = 3)
 
 # correlation between AADT and crash count
-cor(df$AADT, df$COUNT_MILE)
-plot(df$AADT, df$COUNT_MILE)
-abline(lm(df$COUNT_MILE ~ df$AADT), col = 4, lwd = 3)
+cor(df_15$AADT, df_15$COUNT_MILE)
+plot(df_15$AADT, df_15$COUNT_MILE)
+abline(lm(df_15$COUNT_MILE ~ df_15$AADT), col = 4, lwd = 3)
+
+# select route: US-89
+df_89 <- df[df$LABEL == "0089",]
+
+# correlation between SN and crash count
+cor(df_89$SN, df_89$COUNT_MILE)
+plot(df_89$SN, df_89$COUNT_MILE)
+abline(lm(df_89$COUNT_MILE ~ df_89$SN), col = 4, lwd = 3)
+
+# correlation between SN and AADT
+cor(df_89$SN, df_89$AADT)
+plot(df_89$SN, df_89$AADT)
+abline(lm(df_89$AADT ~ df_89$SN), col = 4, lwd = 3)
+
+# correlation between AADT and crash count
+cor(df_89$AADT, df_89$COUNT_MILE)
+plot(df_89$AADT, df_89$COUNT_MILE)
+abline(lm(df_89$COUNT_MILE ~ df_89$AADT), col = 4, lwd = 3)
 
 #------------------------------------------------------------------------------#
 #------------------------------------------------------------------------------#
