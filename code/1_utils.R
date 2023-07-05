e108fccd2cad61bb5758f8af5edf6cd59a41b994
Sailesh <sailesh.acharya@usu.edu>
2023-07-05 15:19:08 -0600

    Formatting changes

--- code/1_utils.R
+++ code/1_utils.R
@@ -1,3 +1,5 @@
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
 # function that returns a list of values between start and end mile points
 # 'start' is the start value
 # 'end' is the end value
@@ -13,9 +15,18 @@ return_range <- function(start, end, int) {
   return(list(c(start, z)))
 }
 
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+
+
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
 # function that returns the value of crash modificaiton factor (CMF)
 # for the variable of interest
 # 'beta' is the estimated coefficient for the variable
 # 'test' is the value of the variable that you want CMF for
-# 'base' is the reference value of variable for CMF calcualation 
+# 'base' is the reference value of variable for CMF calcualation
 return_cmf <- function(beta, test, base) exp(beta * (test - base))
+
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
