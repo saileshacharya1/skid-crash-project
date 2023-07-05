15ce89d28ba376ab27e21ace0242f1803d23ad6a
Sailesh <sailesh.acharya@usu.edu>
2023-07-05 13:49:45 -0600

    Break "main.R" script to multiple scripts

---
+++ code/4_visualizations.R
@@ -0,0 +1,41 @@
+### Some visualizations ########################################################
+################################################################################
+
+# select route: I-15
+df_15 <- df[df$LABEL == "0015N" | df$LABEL == "0015P", ]
+
+# correlation between SN and crash count
+cor(df_15$SN, df_15$COUNT_MILE)
+plot(df_15$SN, df_15$COUNT_MILE)
+abline(lm(df_15$COUNT_MILE ~ df_15$SN), col = 4, lwd = 3)
+
+# correlation between SN and AADT
+cor(df_15$SN, df_15$AADT)
+plot(df_15$SN, df_15$AADT)
+abline(lm(df_15$AADT ~ df_15$SN), col = 4, lwd = 3)
+
+# correlation between AADT and crash count
+cor(df_15$AADT, df_15$COUNT_MILE)
+plot(df_15$AADT, df_15$COUNT_MILE)
+abline(lm(df_15$COUNT_MILE ~ df_15$AADT), col = 4, lwd = 3)
+
+# select route: US-89
+df_89 <- df[df$LABEL == "0089", ]
+
+# correlation between SN and crash count
+cor(df_89$SN, df_89$COUNT_MILE)
+plot(df_89$SN, df_89$COUNT_MILE)
+abline(lm(df_89$COUNT_MILE ~ df_89$SN), col = 4, lwd = 3)
+
+# correlation between SN and AADT
+cor(df_89$SN, df_89$AADT)
+plot(df_89$SN, df_89$AADT)
+abline(lm(df_89$AADT ~ df_89$SN), col = 4, lwd = 3)
+
+# correlation between AADT and crash count
+cor(df_89$AADT, df_89$COUNT_MILE)
+plot(df_89$AADT, df_89$COUNT_MILE)
+abline(lm(df_89$COUNT_MILE ~ df_89$AADT), col = 4, lwd = 3)
+
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
